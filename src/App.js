72c757f8d8ab2ec30f7de9011b3082357df286b9
koderoom <koderoom.dev@gmail.com>
2021-01-18 12:23:27 +0530

    inline styling.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import "./App.css";
 
 /**
- * class => reservered keyword in JS
- * style
+ * class :: className
+ * style :: by using object literals {}
  */
 function App() {
-  return <div className="beafutify">Matrix 4</div>;
+  return (
+    <div
+      style={{
+        fontSize: "2rem",
+        backgroundColor: "royalblue",
+        color: "white",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      Matrix 4
+    </div>
+  );
 }
 
 export default App;
