049348f4d7c087def84ca103d8c1df206edc63fc
koderoom <koderoom.dev@gmail.com>
2021-01-18 11:57:41 +0530

    b2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
-/**
- * Reusable Component
- * <App />
- *
- * Start your component name with Capital Letter
- *
- * Keep your File Name, Start with Capital
- *
- * We Try to put All our HTML inside ()
- * JSX () => Used to Wrtie HTML inside JS.
- *
- * For only one TAG it is OPTINAL :: Try to correate with lamda
- */
 function App() {
   return (
     <div>
-      <div>ABCD</div>
+      <Home />
+
+      <Page1 />
+      <Page1 />
+      <Page1 />
+      <Page1 />
+      <Page1 />
+    </div>
+  );
+}
+
+// <Home />
+function Home() {
+  return <div>Home Component</div>;
+}
+
+function Page2() {
+  return (
+    <div>
+      <div>Abcd</div>
+      <div>Pqrs</div>
+    </div>
+  );
+}
+
+// <Page1 />
+function Page1() {
+  return (
+    <div>
+      <div>
+        ================================================================
+      </div>
+
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Aspernatur dignissimos alias enim laboriosam, molestias sed facilis
+        iure, voluptatum fugiat deserunt non exercitationem sequi hic
+        placeat et explicabo illum repudiandae nihil provident fugit
+        dolores eveniet. Officiis rerum perspiciatis ipsa velit magnam!
+      </div>
     </div>
   );
 }
