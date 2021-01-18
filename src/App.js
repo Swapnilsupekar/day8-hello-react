32db5ed6812caedf7c3ed87c1649639cec74454c
koderoom <koderoom.dev@gmail.com>
2021-01-18 12:03:38 +0530

    component as sprate file

--- src/App.js
+++ src/App.js
@@ -1,46 +1,20 @@
+import Home from "./Home";
+import Page1 from "./Page1";
+
 function App() {
   return (
     <div>
       <Home />
-
       <Page1 />
-      <Page1 />
-      <Page1 />
-      <Page1 />
-      <Page1 />
-    </div>
-  );
-}
 
-// <Home />
-function Home() {
-  return <div>Home Component</div>;
-}
+      <br />
+      <Page1 />
 
-function Page2() {
-  return (
-    <div>
-      <div>Abcd</div>
-      <div>Pqrs</div>
-    </div>
-  );
-}
+      <br />
+      <Page1 />
 
-// <Page1 />
-function Page1() {
-  return (
-    <div>
-      <div>
-        ================================================================
-      </div>
-
-      <div>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-        Aspernatur dignissimos alias enim laboriosam, molestias sed facilis
-        iure, voluptatum fugiat deserunt non exercitationem sequi hic
-        placeat et explicabo illum repudiandae nihil provident fugit
-        dolores eveniet. Officiis rerum perspiciatis ipsa velit magnam!
-      </div>
+      <br />
+      <Page1 />
     </div>
   );
 }
