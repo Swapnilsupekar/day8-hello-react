f132a4f5ec9286d4b2d2529d088275b70a4d623b
koderoom <koderoom.dev@gmail.com>
2021-01-18 11:38:30 +0530

    b1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+/**
+ * Reusable Component
+ * <App />
+ *
+ * Start your component name with Capital Letter
+ *
+ * Keep your File Name, Start with Capital
+ *
+ * We Try to put All our HTML inside ()
+ * JSX () => Used to Wrtie HTML inside JS.
+ *
+ * For only one TAG it is OPTINAL :: Try to correate with lamda
+ */
 function App() {
-  return <div className="App">Hello Universe</div>;
+  return (
+    <div>
+      <div>ABCD</div>
+    </div>
+  );
 }
 
 export default App;
